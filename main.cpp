d28048bde0bf9d5a36dd515a5c5cd686814d693a
Sami-Shafi <freelancersami123@gmail.com>
2025-05-26 02:46:45 +0600

    cin getline, ignore, string function

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 int main()
 {
-    int a,b, c = 9;
-    cin >> a >> b;
-    cout << min(a,b) << endl;
-    cout << max(a,b) << endl;
-    cout << min({a,b,c}) << endl;
+    int x;
+    cin >> x;
+    cin.ignore();
+    char s[100];
+    cin.getline(s, 100);
+    cout << x << endl << s << endl;
     return 0;
 }
