0139776f94568199787ebbfd39d907287d37958f
Sami-Shafi <freelancersami123@gmail.com>
2025-05-28 02:33:20 +0600

    reverse array

--- main.cpp
+++ main.cpp
@@ -1,14 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Given a number N and an array A of N numbers. Print the array in a reversed order.
+
+// Note:
+
+// Don't use built-in-functions.
+
+// Input
+// First line contains a number N (1 ≤ N ≤ 103) number of elements.
+
+// Second line contains N numbers (0 ≤ Ai ≤ 109).
+
 int main()
 {
-    int x, y;
-    cin >> x >> y;
+    int n;
+    cin >> n;
 
-    cout << x << " + " << y << " = " << x+y << endl;
-    cout << x << " * " << y << " = " << x*y << endl;
-    cout << x << " - " << y << " = " << x-y << endl;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    for (int i = 0, j = n-1; i < j; i++, j--)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
     
     return 0;
 } 
