a1c2546acf4dc00a4f75078eeccf51ea77125802
Sami-Shafi <freelancersami123@gmail.com>
2025-05-31 19:44:36 +0600

    sort problem hackerrank

--- main.cpp
+++ main.cpp
@@ -1,33 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
-    
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i+1; j < n; j++)
-        {
-            if(arr[i] > arr[j]) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        
-    }
-    
+// Problem Statement
+
+// You will be given an array A of size N. Initially, you need to print the array by sorting it in ascending order. Afterward, you need to print the array sorted in descending order.
+
+// Input Format
+
+// First line will contain N.
+// Next line will contain the array A.
+// Constraints
+
+// 1 <= N <= 10^5
+// -10^9 <= A[i] <= 10^9 Where 0 <= i < N
+// Output Format
+
+// Print two lines. First line will contain the array sorted in ascending order. Next line will contain the array sorted in descending order.
+// Sample Input 0
+
+// 5
+// 2 4 6 1 3
+// Sample Output 0
+
+// 1 2 3 4 6
+// 6 4 3 2 1
+
+void printArr(int arr[], int n) {
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
-    
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int asc[n], desc[n], val;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> val;
+        asc[i] = val;
+        desc[i] = val;
+    }
+
+    sort(asc, asc+n);
+    sort(desc, desc+n, greater<int>());
+
+    printArr(asc, n);
+    printArr(desc, n);
 
     return 0;
 } 
