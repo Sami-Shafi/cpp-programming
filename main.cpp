328e32af486a15599079549bcf5316d53d1cee3b
Sami-Shafi <freelancersami123@gmail.com>
2025-06-03 05:15:30 +0600

    String Functions 1

--- main.cpp
+++ main.cpp
@@ -1,90 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Problem Statement
-
-// You will be given an array A and the size of that array N. Additionally, you will be given a sum S. Your task is to determine whether it is possible to select three distinct indexed values from the array such that their summation equals S.
-
-// Input Format
-
-// First line will contain T, the number of test cases.
-// First line of each test case will contain N and S.
-// Second line of each test case will contain the array A.
-// Constraints
-
-// 1 <= T <= 100
-// 1 <= N <= 100
-// 1 <= S <= 1000
-// 1 <= A[i] <= 1000 Where 0 <= i < N
-// Output Format
-
-// Output "YES" if it is possible, otherwise output "NO".
-// Sample Input 0
-
-// 5
-// 5 10
-// 1 2 3 4 5
-// 5 6
-// 4 2 3 5 4
-// 3 6
-// 2 2 2
-// 4 4
-// 2 8 1 5
-// 1 3
-// 1
-// Sample Output 0
-
-// YES
-// NO
-// YES
-// NO
-// NO
-// Explanation 0
-
-// In the first test case, we can make 10 by adding 5+4+1. There are other ways too.
-// In the second test case, it is not possible to make 6 by adding three different indexed values from the array.
-// In the third case, it is possible to make 6 by using three different indexed values.
-
 int main()
 {
-    int t;
-    cin >> t;
+    string text = "Hello Sister";
+    string add = "!";
+    // cout << text << endl;
+    // cout << text.size() << endl;
+    // cout << text.max_size() << endl; 
+    // cout << text.capacity() << endl; 
+    // text.clear();
+    // text.resize(16, 'r');
+    // cout << text[0] << endl; 
+    // cout << text.at(0) << endl; 
+    // cout << text.back() << endl; 
+    // cout << text.front() << endl;
+    // text += add;
+    // text.append(add);
+    // text.push_back('A');
+    // text.pop_back();
 
-    while(t--) {
-
-        int size, sum;
-        bool isMatch = false;
-        cin >> size >> sum;
-        int arr[size];
-
-        for (int i = 0; i < size; i++)
-        {
-            cin >> arr[i];
-        }
-
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = i+1; j < size; j++)
-            {
-                for (int k = j+1; k < size; k++)
-                {
-                    if (arr[i] + arr[j] + arr[k] == sum) {
-                        isMatch = true;
-                        break;
-                    }
-                }
-                
-            }
-        }
-
-        if(isMatch == true) {
-            cout << "YES";
-        }else {
-            cout << "NO";
-        }
-        cout << endl;
-        
-    }
+    cout << text << endl; 
 
     return 0;
 } 
