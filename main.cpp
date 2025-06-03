c86263c12947b8886757aa76fd430e437a93e9f6
Sami-Shafi <freelancersami123@gmail.com>
2025-06-04 03:55:59 +0600

    iterators

--- main.cpp
+++ main.cpp
@@ -4,23 +4,10 @@ using namespace std;
 int main()
 {
     string text = "Hello Sister";
-    string add = "!";
-    // cout << text << endl;
-    // cout << text.size() << endl;
-    // cout << text.max_size() << endl; 
-    // cout << text.capacity() << endl; 
-    // text.clear();
-    // text.resize(16, 'r');
-    // cout << text[0] << endl; 
-    // cout << text.at(0) << endl; 
-    // cout << text.back() << endl; 
-    // cout << text.front() << endl;
-    // text += add;
-    // text.append(add);
-    // text.push_back('A');
-    // text.pop_back();
-
-    cout << text << endl; 
+    
+    for (auto it = text.begin(); it < text.end(); it++) {
+        cout << *it;
+    }
 
     return 0;
 } 
