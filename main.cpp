372e13886c57aa3f2655e86c41e0771082242891
Sami-Shafi <freelancersami123@gmail.com>
2025-06-05 03:32:20 +0600

    functions in class

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Citizen {
+    public:
+    string name, blood;
+    int age;
+    bool marriageStatus;
+
+    Citizen(string name, int age, string blood, bool marriageStatus) {
+        this->name = name;
+        this->age = age;
+        this->blood = blood;
+        this->marriageStatus = marriageStatus;
+    }
+
+    void intro() {
+        cout << "My name is " << name << ". I am " << age << " and I'm happily " << ((marriageStatus) ? "married" : "single") << ". My blood group is " << blood << "." << endl << endl;
+    }
+};
+
 int main()
 {
-    int nums[5];
-    
-    for (int i = 0; i < 5; i++) {
-        cin >> nums[i];
-    }
+    Citizen Dinan("Medri Ferdinand", 38, "O+", true);
+    Citizen Nep("Nepoleon Enzo", 25, "AB+", false);
+    Citizen Trudo("Justin Trudo", 46, "A+", true);
 
-    reverse(nums, nums+5);
-
-    for (int i = 0; i < 5; i++)
-    {
-        cout << nums[i] << " ";
-    }
-    
+    Dinan.intro();
+    Nep.intro();
+    Trudo.intro();
 
     return 0;
 } 
