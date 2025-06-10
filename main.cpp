84401d69b9f46db2694ba24139a0eed5d1004ae2
Sami-Shafi <freelancersami123@gmail.com>
2025-06-10 20:24:34 +0600

    word makeable character counter

--- main.cpp
+++ main.cpp
@@ -1,67 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// A ticket is a string consisting of six digits. A ticket is considered lucky if the sum of the first three digits is equal to the sum of the last three digits. Given a ticket, output if it is lucky or not. Note that a ticket can have leading zeroes.
+// Given a string S. Print number of times that "EGYPT" word can be formed from S's characters.
+
+// Note: Case of the letters doesn't matter. For example: "Egypt", "egypt" and "eGyPt" are the same.
 
 // Input
-// The first line of the input contains an integer t
-//  (1≤t≤103) — the number of testcases.
-
-// The description of each test consists of one line containing one string consisting of six digits.
-
-// Output
-// Output t lines, each of which contains the answer to the corresponding test case. Output "YES" if the given ticket is lucky, and "NO" otherwise.
-
-// You can output the answer in any case (for example, the strings "yEs", "yes", "Yes" and "YES" will be recognized as a positive answer).
-
-// Example
-
-// Input
-// 5
-// 213132
-// 973894
-// 045207
-// 000000
-// 055776
-
-// Output
-// YES
-// NO
-// YES
-// YES
-// NO
-
-int sumOfDigits(int DigitBlock) {
-    int sum = 0;
-    while (DigitBlock > 0) {
-        sum += DigitBlock % 10;
-        DigitBlock /= 10;
-    }
-    return sum;
-}
+// Only one line contains a string S(1≤|S|≤106) where |S| is the length of the string and it consists of lowercase and uppercase English letters.
 
 int main()
 {
-    int t;
-    cin >> t;
+    string text;
+    int counts[100] = {0};
+    cin >> text;
 
-    for (int i = 0; i < t; i++)
+    for (int i = 0; i < text.size(); i++)
     {
-        long long int num;
-        cin >> num;
-
-        int first3 = num / 1000;
-        int last3 = num % 1000;
-
-        if (sumOfDigits(first3) == sumOfDigits(last3)) {
-            cout << "YES";
-        } else {
-            cout << "NO";
+        char current = toupper(text[i]);
+        if (current == 'E' || current == 'G' || current == 'Y' || current == 'P' || current == 'T') {
+            counts[int(current)]++;
         }
-
-        cout << endl;
     }
-    
 
+    cout << min({counts['E'], counts['G'], counts['Y'], counts['P'], counts['T']});
+    
     return 0;
 } 
