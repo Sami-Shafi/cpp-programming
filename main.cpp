3eda7b0e53b88569f8fa3da59ef473543c339704
Sami-Shafi <freelancersami123@gmail.com>
2025-06-11 08:37:04 +0600

    Array of Objects

--- main.cpp
+++ main.cpp
@@ -1,28 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Given a string S. Print number of times that "EGYPT" word can be formed from S's characters.
-
-// Note: Case of the letters doesn't matter. For example: "Egypt", "egypt" and "eGyPt" are the same.
-
-// Input
-// Only one line contains a string S(1≤|S|≤106) where |S| is the length of the string and it consists of lowercase and uppercase English letters.
+class Student {
+    public:
+    string name;
+    int roll;
+    int marks;
+};
 
 int main()
 {
-    string text;
-    int counts[100] = {0};
-    cin >> text;
+    int n;
+    cin >> n;
+    Student arr[n];
 
-    for (int i = 0; i < text.size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        char current = toupper(text[i]);
-        if (current == 'E' || current == 'G' || current == 'Y' || current == 'P' || current == 'T') {
-            counts[int(current)]++;
-        }
+        cin.ignore();
+        getline(cin, arr[i].name);
+        cin >> arr[i].roll >> arr[i].marks;
     }
 
-    cout << min({counts['E'], counts['G'], counts['Y'], counts['P'], counts['T']});
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i].name << " " << arr[i].roll << " " << arr[i].marks << endl;
+    }
+    
     
     return 0;
 } 
