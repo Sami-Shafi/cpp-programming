03fb4532f8de49f52fa8ec29dd5f562fead173fe
Sami-Shafi <freelancersami123@gmail.com>
2025-06-14 05:56:00 +0600

    Hackerrank Array of Objects Sorting Supper Complex

--- main.cpp
+++ main.cpp
@@ -3,54 +3,56 @@ using namespace std;
 
 // You will be given data for N students, where each student will have a name (nm), class (cls), section (s), student ID (id), math marks (math_marks), and English marks (eng_marks).
 
-// Your task is to sort the students data according to the total marks (sum of math_marks and eng_marks) in descending order. If multiple student have the same total marks then sort them according to the id in ascending order as the id will be unique.
+// Your task is to sort the students data according to the eng_marks in descending order. If multiple student have the same eng_marks then sort them according to the math_marks in descending order. If multiple student have the same math_marks then sort them accoding to the id in ascending order as the id will be unique.
 
 // Input Format
 // First line will contain N.
 // Next N lines will contain nm, cls, s, id, math_marks and eng_marks respectively.
 
 // Constraints
-// 1 <= N <= 100
+// 1 <= N <= 1000
 // 1 <= |nm| <= 100 and will contain only English alphabets.
 // 1 <= cls <= 10
 // 'A' <= s <= 'Z'
-// 1 <= id <= 1000
+// 1 <= id <= 10^9
 // 0 <= math_marks, eng_marks <= 100
 
 // Output Format
-// Output the students data in descending order according to the total marks.
+// Output the data in sorted order as instructed.
 
 // Sample Input 0
-// 5
-// Munna 8 D 25 50 30
-// Shojoy 9 E 26 40 50
-// Asif 10 C 27 55 60
-// Joy 9 G 28 66 45
-// Bijoy 7 E 29 68 99
+// 6
+// akib 2 R 1001 32 53
+// rakib 1 E 1002 93 97
+// sakib 8 M 1003 34 88
+// bokib 3 Q 1004 93 58
+// jessica 4 F 1005 94 88
+// noname 8 R 1006 17 61
 
 // Sample Output 0
-// Bijoy 7 E 29 68 99
-// Asif 10 C 27 55 60
-// Joy 9 G 28 66 45
-// Shojoy 9 E 26 40 50
-// Munna 8 D 25 50 30
+// rakib 1 E 1002 93 97
+// jessica 4 F 1005 94 88
+// sakib 8 M 1003 34 88
+// noname 8 R 1006 17 61
+// bokib 3 Q 1004 93 58
+// akib 2 R 1001 32 53
 
 // Sample Input 1
 // 6
-// Munna 8 D 30 50 40
-// Shojoy 9 E 25 40 50
-// Asif 10 C 27 55 60
-// Joy 9 G 28 66 45
-// Bijoy 7 E 29 68 99
-// Khadija 8 E 26 40 50
+// akib 2 R 1001 32 53
+// rakib 1 E 1002 94 88
+// sakib 8 M 1003 34 88
+// bokib 3 Q 1004 93 58
+// jessica 4 F 1005 94 88
+// noname 8 R 1006 17 61
 
 // Sample Output 1
-// Bijoy 7 E 29 68 99
-// Asif 10 C 27 55 60
-// Joy 9 G 28 66 45
-// Shojoy 9 E 25 40 50
-// Khadija 8 E 26 40 50
-// Munna 8 D 30 50 40
+// rakib 1 E 1002 94 88
+// jessica 4 F 1005 94 88
+// sakib 8 M 1003 34 88
+// noname 8 R 1006 17 61
+// bokib 3 Q 1004 93 58
+// akib 2 R 1001 32 53
 
 class Student {
     public:
@@ -64,16 +66,19 @@ class Student {
 };
 
 bool studentSorting (Student prevStudent, Student nextStudent) {
-    int prevMarks = prevStudent.mm + prevStudent.em;
-    int nextMarks = nextStudent.mm + nextStudent.em;
+    if (prevStudent.em < nextStudent.em) {
+        return false;
+    }else if (prevStudent.em == nextStudent.em) {
+        if (prevStudent.mm < nextStudent.mm) {
+            return false;
+        } else if (prevStudent.mm == nextStudent.mm) {
+            if (prevStudent.id > nextStudent.id) {
+                return false;
+            }
+        }
+    }
 
-    return (prevMarks < nextMarks)
-    ? false :
-    (prevMarks == nextMarks) ?
-    (
-        (prevStudent.id > nextStudent.id) ? false : true
-    )
-    : true;
+    return true;
 }
 
 int main()
